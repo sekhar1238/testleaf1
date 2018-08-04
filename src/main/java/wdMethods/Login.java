570b15e9ebf90e8a1e9905839cb663e483554182
Sekhk <Sekhk@Sekhar>
2018-08-04 17:42:19 +0530

    first comt

---
+++ src/main/java/wdMethods/Login.java
@@ -0,0 +1,39 @@
+package wdMethods;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import wdMethods.SeMethods;
+
+public class Login extends SeMethods{
+
+	@Test
+	public void login() {
+		startApp("chrome", "http://leaftaps.com/opentaps");
+		WebElement eleUserName = locateElement("id", "username");
+		type(eleUserName, "DemoSalesManager");
+		WebElement elePassword = locateElement("id","password");
+		type(elePassword, "crmsfa");
+		WebElement eleLogin = locateElement("class","decorativeSubmit");
+		click(eleLogin);
+		WebElement eleCRMSFA=locateElement("linktext","CRM/SFA");
+		click(eleCRMSFA);
+		WebElement eleCreateLead=locateElement("linktext","Create Lead");
+		click(eleCreateLead);
+		WebElement eleCompName=locateElement("id","createLeadForm_companyName");
+		type(eleCompName,"HCL Technologies");
+		WebElement eleFirstName=locateElement("id","createLeadForm_firstName");
+		type(eleFirstName,"Sekhar");
+		WebElement eleLastName=locateElement("id","createLeadForm_lastName");
+		type(eleLastName,"k");
+		WebElement eleCurrencyDD=locateElement("id","createLeadForm_currencyUomId");
+		selectDropDownUsingIndex(eleCurrencyDD, 4);
+	}
+
+}
+
+
+
+
+
+
